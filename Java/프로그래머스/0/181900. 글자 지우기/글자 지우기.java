9bd5122ec18ada22c409c198e6daaf1e3f6b5a9b
Rumanistic <70498096+Rumanistic@users.noreply.github.com>
2024-05-23 12:29:18 +0900

    [level 0] Title: 글자 지우기, Time: 0.48 ms, Memory: 74.4 MB -BaekjoonHub

--- Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ Java/프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,16 +1,22 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
         StringBuilder sb = new StringBuilder();
-        char[] str = my_string.toCharArray();
+        // char[] str = my_string.toCharArray();
+        String[] str = my_string.split("");
         
         for(int i : indices){
-            str[i] = ' ';
+            // str[i] = ' ';
+            str[i] = "";
         }
         
-        for(char c : str){
-            if (c != ' '){
-                sb.append(c);
-            }
+        // for(char c : str){
+        //     if (c != ' '){
+        //         sb.append(c);
+        //     }
+        // }
+        
+        for(String s : str){
+            sb.append(s);
         }
         
         return sb.toString();
