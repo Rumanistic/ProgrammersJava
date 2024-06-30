dce3d524749db2d6b0fad66bdf3ecdd782d1c412
Rumanistic <70498096+Rumanistic@users.noreply.github.com>
2024-06-30 19:27:22 +0900

    [Bronze V] Title: 연세대학교, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ Java/백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        switch(sc.nextInt()){
+            case 0:
+                System.out.println("YONSEI");
+                break;
+            case 1:
+                System.out.println("Leading the Way to the Future");
+                break;
+        }
+    }
+}
