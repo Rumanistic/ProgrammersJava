ed3918707b52b9e571a4517fdcdea08fe8533d5d
Rumanistic <70498096+Rumanistic@users.noreply.github.com>
2024-07-01 15:51:29 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int count = 0;
+        
+        for(int i : array){
+            if(i == n)
+                count++;
+        }
+        
+        return count;
+    }
+}
